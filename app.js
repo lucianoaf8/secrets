ee8156a2c68dfd9918214dbb4b1b393100c649e9
Luciano <lucianoaf8@gmail.com>
2020-05-06 20:13:23 -0600

    Work on submit a secret

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
+    secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -103,8 +104,20 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+    User.find({ secret: { $ne: null } }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers });
+            }
+        }
+    });
+});
+
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
@@ -147,6 +160,23 @@ app.post("/login", function (req, res) {
             passport.authenticate("local")(req, res, function () {
                 res.redirect("/secrets");
             });
+        }
+    });
+});
+
+app.post("/submit", function (req, res) {
+    req.body.secret;
+
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = req.body.secret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
         }
     });
 });
